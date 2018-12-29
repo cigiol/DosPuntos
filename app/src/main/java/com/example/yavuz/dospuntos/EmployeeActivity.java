129ce3726716435982fe3af97756a6e086d9b838
mustafaerk <34791079+mustafaerk@users.noreply.github.com>
2018-12-30 02:28:19 +0300

    new push

--- app/src/main/java/com/example/yavuz/dospuntos/EmployeeActivity.java
+++ app/src/main/java/com/example/yavuz/dospuntos/EmployeeActivity.java
@@ -1,40 +1,14 @@
 package com.example.yavuz.dospuntos;
 
-        import android.app.Activity;
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.Toast;
-
-        import com.google.firebase.database.DataSnapshot;
-        import com.google.firebase.database.DatabaseError;
-        import com.google.firebase.database.DatabaseReference;
-        import com.google.firebase.database.FirebaseDatabase;
-        import com.google.firebase.database.ValueEventListener;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class EmployeeActivity extends Activity {
-
-    private Button Setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        Setting = findViewById(R.id.employeeSettingsBtn);
-
-        //Login
-        Setting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intocan = new Intent(EmployeeActivity.this, EmployeeSettingsActivity.class);
-                startActivity(intocan);
-                 }
-            });
+        setContentView(R.layout.activity_employee);
     }
 }
